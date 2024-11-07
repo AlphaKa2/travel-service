f079fce65a3278a7e338e28fae5db4fabb9812c1
Hojun Im <ghwnsdla8094@gmail.com>
2024-11-07 22:12:35 +0900

    AK-222 [Feat] 사용자 ID 리스트로 정보 조회 feign client 호출 추가

--- src/main/java/com/alphaka/travelservice/client/UserClient.java
+++ src/main/java/com/alphaka/travelservice/client/UserClient.java
@@ -4,7 +4,11 @@ import com.alphaka.travelservice.common.dto.UserDTO;
 import com.alphaka.travelservice.common.response.ApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * 사용자 정보 조회를 위한 Feign Client
@@ -19,4 +23,12 @@ public interface UserClient {
     // ID로 사용자 조회
     @GetMapping("/users/info")
     ApiResponse<UserDTO> findUserById(@RequestParam("id") Long id);
+
+    /**
+     * 사용자 ID 목록으로 사용자 조회 (user-service에서 결정되는 API Path에 따라 변경될 수 있음)
+     * @param userIds - 사용자 ID 목록
+     * @return ApiResponse<List<UserDTO>> - 사용자 정보 목록
+     */
+    @GetMapping("/users/{userId}/all")
+    ApiResponse<List<UserDTO>> getUsersById(@PathVariable("userId") Set<Long> userIds);
 }
