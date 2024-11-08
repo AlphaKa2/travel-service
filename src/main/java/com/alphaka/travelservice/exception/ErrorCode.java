31c679ab7c2d507885c10468f3269a4478a82ac6
Hojun Im <ghwnsdla8094@gmail.com>
2024-11-08 22:30:41 +0900

    AK-261 [Fix] 예외 알맞게 수정

--- src/main/java/com/alphaka/travelservice/exception/ErrorCode.java
+++ src/main/java/com/alphaka/travelservice/exception/ErrorCode.java
@@ -16,14 +16,11 @@ public enum ErrorCode {
     // Travel
     PLAN_NOT_FOUND(404, "TRV001", "존재하지 않는 여행입니다."),
     INVALID_TRAVEL_DAY(400, "TRV002", "여행 일자가 올바르지 않습니다."),
-    INVALID_TRAVEL_STATUS(400, "TRV003", "여행 상태가 올바르지 않습니다."),
-    REVIEW_ALREADY_WRITTEN(400, "TRV004", "이미 리뷰를 작성한 여행입니다."),
-
-    // Participant
-    PARTICIPANT_NOT_FOUND(404, "PAL001", "존재하지 않는 참여 정보 입니다."),
-
-    // Invitation
-    INVITATION_NOT_FOUND(404, "INV001", "존재하지 않는 초대자 정보 입니다.");
+    INVALID_TRAVEL_SCHEDULE(400, "TRV003", "여행 스케줄이 올바르지 않습니다."),
+    INVALID_TRAVEL_STATUS(400, "TRV004", "여행 상태가 올바르지 않습니다."),
+    REVIEW_ALREADY_WRITTEN(400, "TRV005", "이미 리뷰를 작성한 여행입니다."),
+    PARTICIPANT_NOT_FOUND(404, "TRV006", "존재하지 않는 참여 정보 입니다."),
+    INVITATION_NOT_FOUND(404, "TRV007", "존재하지 않는 초대자 정보 입니다.");
 
     private final int status;
     private final String code;
